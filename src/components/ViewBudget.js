22314b452fb3e0b6b26d7b8f4247bbc7a4b502ec
andrzejb9 <a.bankowski9@o2.pl>
2023-03-16 21:17:52 +0100

    budget unit change

--- src/components/ViewBudget.js
+++ src/components/ViewBudget.js
@@ -3,7 +3,7 @@ import React from "react";
 function ViewBudget({ budget, handleEditClick }) {
   return (
     <>
-      <span>Budget: £{budget}</span>
+      <span>Budget: ${budget}</span>
       <button
         type="button"
         className="btn btn-primary"
